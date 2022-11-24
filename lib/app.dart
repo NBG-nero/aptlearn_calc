8bf0dc6675c0874d657528004ef9ebdbafe4bb49
Aphra <oreofeojomo@gmail.com>
2022-11-24 14:57:16 +0100

    feat: project setup

---
+++ lib/app.dart
@@ -0,0 +1,19 @@
+import 'package:aptlearn_calc/screens/calc_screen.dart';
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+ 
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Aptlearn Calc',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const CalcScreen(),
+    );
+  }
+}
---
+++ lib/screens/calc_screen.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class CalcScreen extends StatefulWidget {
+  const CalcScreen({super.key});
+
+  @override
+  State<CalcScreen> createState() => _CalcScreenState();
+}
+
+class _CalcScreenState extends State<CalcScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black87,
+      body: SafeArea(child: SingleChildScrollView(child: Column(
+        children: [ 
+
+          
+        ],
+      ))),
+    );
+  }
+}
