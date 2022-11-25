ff2ac0c8d6b42e829a547e4dd475580c6166ff4e
Aphra <oreofeojomo@gmail.com>
2022-11-25 21:28:53 +0100

    feat: minor changes

--- lib/screens/calc_screen.dart
+++ lib/screens/calc_screen.dart
@@ -12,10 +12,21 @@ class _CalcScreenState extends State<CalcScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.black87,
-      body: SafeArea(child: SingleChildScrollView(child: Column(
-        children: [ 
-
-          
+      body: SafeArea(
+          child: SingleChildScrollView(
+              child: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          const Text(
+            "0",
+            style: TextStyle(color: Colors.white, fontSize: 50),
+          ),
+          const Text(
+            "200",
+            style: TextStyle(color: Colors.white, fontSize: 30),
+          ),
+          Row(),
         ],
       ))),
     );
---
+++ lib/widgets/custom_button.dart
@@ -0,0 +1,17 @@
+
+
+import 'package:flutter/material.dart';
+
+class CustomBotton extends StatefulWidget {
+  const CustomBotton({super.key});
+
+  @override
+  State<CustomBotton> createState() => _CustomBottonState();
+}
+
+class _CustomBottonState extends State<CustomBotton> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
