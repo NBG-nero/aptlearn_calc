d3392058161347abaf0c2452541786c4dcfc37a7
Aphra <oreofeojomo@gmail.com>
2022-11-26 23:56:44 +0100

    custombutton inprogress

--- lib/screens/calc_screen.dart
+++ lib/screens/calc_screen.dart
@@ -1,3 +1,4 @@
+import 'package:aptlearn_calc/widgets/widgets.dart';
 import 'package:flutter/material.dart';
 
 class CalcScreen extends StatefulWidget {
@@ -13,22 +14,51 @@ class _CalcScreenState extends State<CalcScreen> {
     return Scaffold(
       backgroundColor: Colors.black87,
       body: SafeArea(
-          child: SingleChildScrollView(
-              child: Column(
-        mainAxisAlignment: MainAxisAlignment.end,
-        crossAxisAlignment: CrossAxisAlignment.end,
-        children: [
-          const Text(
-            "0",
-            style: TextStyle(color: Colors.white, fontSize: 50),
-          ),
-          const Text(
-            "200",
-            style: TextStyle(color: Colors.white, fontSize: 30),
-          ),
-          Row(),
-        ],
-      ))),
+          child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            const Text(
+              "0",
+              style: TextStyle(color: Colors.white, fontSize: 50),
+            ),
+            const Text(
+              "200",
+              style: TextStyle(color: Colors.white, fontSize: 30),
+            ),
+            Row(
+              children: [
+                CustomBotton(
+                  buttonText: "C",
+                  buttonColor: Colors.grey,
+                  onPressed: () {},
+                  textColor: Colors.white,
+                ),
+                CustomBotton(
+                  buttonText: "del",
+                  buttonColor: Colors.grey,
+                  onPressed: () {},
+                  textColor: Colors.white,
+                ),
+                CustomBotton(
+                  buttonText: "%",
+                  buttonColor: Colors.grey,
+                  onPressed: () {},
+                  textColor: Colors.white,
+                ),
+                CustomBotton(
+                  buttonText: "/",
+                  buttonColor: Colors.orange,
+                  onPressed: () {},
+                  textColor: Colors.white,
+                ),
+              ],
+            ),
+          ],
+        ),
+      )),
     );
   }
 }
--- lib/widgets/custom_button.dart
+++ lib/widgets/custom_button.dart
@@ -1,17 +1,32 @@
-
-
 import 'package:flutter/material.dart';
 
-class CustomBotton extends StatefulWidget {
-  const CustomBotton({super.key});
-
-  @override
-  State<CustomBotton> createState() => _CustomBottonState();
-}
-
-class _CustomBottonState extends State<CustomBotton> {
+class CustomBotton extends StatelessWidget {
+  const CustomBotton({
+    Key? key,
+    required this.buttonColor,
+    required this.buttonText,
+    required this.textColor,
+    required this.onPressed,
+  }) : super(key: key);
+  final Color buttonColor;
+  final String buttonText;
+  final Color textColor;
+  final VoidCallback onPressed;
   @override
   Widget build(BuildContext context) {
-    return Container();
+    final size = MediaQuery.of(context).size;
+    return SizedBox(
+      width: size.height * 0.08,
+      height: size.height * 0.08,
+      child: ElevatedButton(
+        style: TextButton.styleFrom(
+          backgroundColor: buttonColor,
+          shape: const CircleBorder(),
+        ),
+        onPressed: onPressed,
+        child:
+            Text(buttonText, style: TextStyle(color: textColor, fontSize: 25)),
+      ),
+    );
   }
 }
